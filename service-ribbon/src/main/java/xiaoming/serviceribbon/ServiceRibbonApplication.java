b54dd93476eacb811e64a70b9d0f2e152ce3ec41
shuihai <99359473@qq.com>
2018-08-05 23:02:43 +0800

    3

---
+++ service-ribbon/src/main/java/xiaoming/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,12 @@
+package xiaoming.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRibbonApplication.class, args);
+	}
+}
